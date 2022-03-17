f47f9b3407f1d32c2811963b72693adead21d8ec
Skillcet <skillsntools.web@gmail.com>
2022-03-17 08:31:27 +0530

    s

--- components/Form.js
+++ components/Form.js
@@ -1,175 +1,206 @@
-import React,{useState} from 'react'
-import {useFormik} from 'formik'
-import styles from '../styles/Form.module.css'
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import styles from "../styles/Form.module.css";
+import CloseIcon from "@mui/icons-material/Close";
 // import {useEffect} from 'react'
-import axios from 'axios';
+import axios from "axios";
 const brandOpt = [
-    "Mangalagiri",
-    "Coimbatore ",
-    "Bengal",
-    "Kota",
-    "Chanderi",
-    "Benaras",
-    "Raw Silk",
-    "Bedsheets",
-    "Tussar",
-    "Pathuru",
-    "Venkatagiri",
-    "Kanchi",
-    "Mul-Mul",
-    "Block-Print",
-    "Maheshwari",
-    "Soft-Silk",
-    "Narayanpet",
-    "Linen",
-    "Linen-Fancy"
-]
+  "Mangalagiri",
+  "Coimbatore ",
+  "Bengal",
+  "Kota",
+  "Chanderi",
+  "Benaras",
+  "Raw Silk",
+  "Bedsheets",
+  "Tussar",
+  "Pathuru",
+  "Venkatagiri",
+  "Kanchi",
+  "Mul-Mul",
+  "Block-Print",
+  "Maheshwari",
+  "Soft-Silk",
+  "Narayanpet",
+  "Linen",
+  "Linen-Fancy",
+];
 const materialOpt = [
-    "Silk-cotton",
-    "Pattu",
-    "Cotton",
-    "Dress-Material",
-    "Cotton-Jamdhani",
-    "Pure-silk",
-    "Benaras Fancy",
-    "Tussar",
-    "Semi Silk",
-    "Shiffon",
-    "Crepe"
-]
+  "Silk-cotton",
+  "Pattu",
+  "Cotton",
+  "Dress-Material",
+  "Cotton-Jamdhani",
+  "Pure-silk",
+  "Benaras Fancy",
+  "Tussar",
+  "Semi Silk",
+  "Shiffon",
+  "Crepe",
+];
 const vendorOpt = [
-    "MA-LAXMI",
-    "CO-SWASTHIKA",
-    "BE-BENGAL HANDLOOMS",
-    "HY-Neelkamal",
-    "KO-Kotasarees",
-    "CH- RiddhimaSarees",
-    "BS - Pandey Sarees",
-    "BS- Nandini Boutique",
-    "RS - Nandini Boutique",
-    "KO - Nandini Boutique",
-    "NE- P.M. Cotton Centre",
-    "JA - Riddhima Sarees",
-    "TR - Nandini Boutique",
-    "PA - Sridevi",
-    "VE - Lakshmipati",
-    "VE - Venkataramaya and sons",
-    "CT - Oswal Saree Centre",
-    "MH- Sainath ",
-    "BE - Nandini Boutique",
-    "KO - Asif Sarees",
-    "SS - Nandini Boutique",
-    "NA - Basude and Bros.",
-    "LN - Nandini Boutique",
-    "CH - Nandini Boutique",
-    "SH - Nandini Boutique",
-    "CR - Nandini Boutique"
-]
-const statusOpt = 
-[
-    "Sold Out",
-    "In Stock",
-    "Reserved",
-    "coming soon"
-]
+  "MA-LAXMI",
+  "CO-SWASTHIKA",
+  "BE-BENGAL HANDLOOMS",
+  "HY-Neelkamal",
+  "KO-Kotasarees",
+  "CH- RiddhimaSarees",
+  "BS - Pandey Sarees",
+  "BS- Nandini Boutique",
+  "RS - Nandini Boutique",
+  "KO - Nandini Boutique",
+  "NE- P.M. Cotton Centre",
+  "JA - Riddhima Sarees",
+  "TR - Nandini Boutique",
+  "PA - Sridevi",
+  "VE - Lakshmipati",
+  "VE - Venkataramaya and sons",
+  "CT - Oswal Saree Centre",
+  "MH- Sainath ",
+  "BE - Nandini Boutique",
+  "KO - Asif Sarees",
+  "SS - Nandini Boutique",
+  "NA - Basude and Bros.",
+  "LN - Nandini Boutique",
+  "CH - Nandini Boutique",
+  "SH - Nandini Boutique",
+  "CR - Nandini Boutique",
+];
+const statusOpt = ["Sold Out", "In Stock", "Reserved", "coming soon"];
 
-function Form({open,setOpen,data}) {
-    
-    // console.log(filterOptions)
-    const [img,setImg] = useState('');
-    const [imgUrll,setImgUrl] = useState('');
-    const array = ['productCode','description','arrivalDate','costPrice','salePrice','soldPrice','customerName','customerContact','amountPaid','dues']
-    const dropDownsOpts = [brandOpt,materialOpt,vendorOpt,statusOpt]
-    const dropDownsMenus = [{name: 'brand',items:brandOpt},{name:'material',items: materialOpt},{name: 'vendor',items:vendorOpt},{name: 'status',items:statusOpt}]
-    const formik = useFormik({
-        initialValues : {
-            productCode : '',
-            brand : '',	
-            material : '',
-            description	: '',
-            arrivalDate	: '',
-            vendor	:'',
-            costPrice	:'',
-            salePrice	:'',
-            soldPrice	:'',
-            customerName	:'',
-            customerContact	: '',
-            dateSold : '',	
-            status	: '',
-            amountPaid :'',	
-            dues :'',
-
-        },
-        onSubmit: async (values) => {
-            let result = (values)
-            result.imgUrl = imgUrll
-            // axios.post(`http://localhost:3000/api/products`, result)
-            // .then(res => {
-            //   console.log(res);
-            //   console.log(res.data);
-            // })
-            console.log(result)
-        }
-    })
-    const handleOpen =()=>{
-        setOpen(!open)
+function Form({ open, setOpen, data }) {
+  // console.log(filterOptions)
+  const [img, setImg] = useState("");
+  const [imgUrll, setImgUrl] = useState("");
+  const array = [
+    "productCode",
+    "description",
+    "arrivalDate",
+    "costPrice",
+    "salePrice",
+    "soldPrice",
+    "customerName",
+    "customerContact",
+    "amountPaid",
+    "dues",
+  ];
+  const dropDownsOpts = [brandOpt, materialOpt, vendorOpt, statusOpt];
+  const dropDownsMenus = [
+    { name: "brand", items: brandOpt },
+    { name: "material", items: materialOpt },
+    { name: "vendor", items: vendorOpt },
+    { name: "status", items: statusOpt },
+  ];
+  const formik = useFormik({
+    initialValues: {
+      productCode: "",
+      brand: "",
+      material: "",
+      description: "",
+      arrivalDate: "",
+      vendor: "",
+      costPrice: "",
+      salePrice: "",
+      soldPrice: "",
+      customerName: "",
+      customerContact: "",
+      dateSold: "",
+      status: "",
+      amountPaid: "",
+      dues: "",
+    },
+    onSubmit: async (values) => {
+      let result = values;
+      result.imgUrl = imgUrll;
+      try {
+        axios.post(`http://localhost:3000/api/products`, result);
+      } catch (err) {
+        alert(arr);
       }
-    const handleImg =(e) =>{
-        setImg(e.target.files[0])
+    },
+  });
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const handleImg = (e) => {
+    setImg(e.target.files[0]);
+  };
+  const handleImgSub = async () => {
+    const form = new FormData();
+    form.append("file", img);
+    form.append("upload_preset", "einoyqyf");
+    try {
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/dbrfc0ryp/image/upload",
+        form
+      );
+      setImgUrl(res.data.url);
+      alert("success");
+    } catch (err) {
+      console.log(err);
     }
-    const handleImgSub = async () =>{
-        const form = new FormData()
-        form.append('file', img)
-        form.append('upload_preset', 'einoyqyf')
-        try {
-        const res = await axios.post(
-            'https://api.cloudinary.com/v1_1/dbrfc0ryp/image/upload',
-            form
-        )
-        setImgUrl(res.data.url)
-        alert('success')
-        } catch (err) {
-        console.log(err)
-        }
-    }
+  };
 
   return (
-      
     <div className={styles.container}>
-        <div onClick={handleOpen} className={styles.closeContainer}><CloseIcon /></div>
-        <div className={styles.formContainer}>
+      <div className={styles.formContainer}>
+        <div onClick={handleOpen} className={styles.closeContainer}>
+          <CloseIcon />
+        </div>
         <form onSubmit={formik.handleSubmit}>
-        {array.map((item,index)=>(
-            <div className={styles.inputContainer} key={index}>     
-            <label htmlFor={item} className={styles.label}>{item.toUpperCase()}</label>
-                <input
+          {array.map((item, index) => (
+            <div className={styles.inputContainer} key={index}>
+              <label htmlFor={item} className={styles.label}>
+                {item.toUpperCase()}
+              </label>
+              <input
                 className={styles.input}
                 id={item}
                 name={item}
                 type={item}
                 onChange={formik.handleChange}
                 value={formik.values.item}
-            />
+              />
             </div>
-        ))}
-        {dropDownsMenus.map((item,index)=>(
-            <div className={styles.inputContainer} key={index}>     
-            <label htmlFor={item.name} className={styles.label}>{item.name.toUpperCase()}</label>
-            <select id={item.name} name={item.name} onChange={formik.handleChange} className={styles.input} value={formik.values.item}>
-                {item.items.map((a,index)=>(
-                    <option value={formik.values.a} key={index}>{a}</option>
+          ))}
+          {dropDownsMenus.map((item, index) => (
+            <div className={styles.inputContainer} key={index}>
+              <label htmlFor={item.name} className={styles.label}>
+                {item.name.toUpperCase()}
+              </label>
+              <select
+                id={item.name}
+                name={item.name}
+                onChange={formik.handleChange}
+                className={styles.input}
+                value={formik.values.item}
+              >
+                {item.items.map((a, index) => (
+                  <option value={formik.values.a} key={index}>
+                    {a}
+                  </option>
                 ))}
-                
-            </select>
+              </select>
             </div>
-        ))}
-        <input type= "file" onChange={event => handleImg(event)}/>
-        <div onClick={handleImgSub}>Upload</div>
-        <button type="submit">Submit</button>
-     </form>
+          ))}
+          <div className={styles.imgUpload}>
+            <input
+              type="file"
+              className={styles.fileInput}
+              onChange={(event) => handleImg(event)}
+            />
+            <div onClick={handleImgSub} className={styles.uploadButton}>
+              Upload
+            </div>
+          </div>
+          <div className={styles.submitContainer}>
+            <button type="submit" className={styles.submitButton}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-export default Form
+export default Form;
--- pages/categories/[id].js
+++ pages/categories/[id].js
@@ -1,83 +1,111 @@
-import React from 'react'
-import Link from 'next/link'
-import { useState} from 'react';
-import { SareeItem } from '../../components/SareeItem';
-import { useRouter } from 'next/router'
-import styles from '../../styles/BrandPage.module.css'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-const Place = ({data}) => {
-  const [mat,setMat] = useState();
+import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import { SareeItem } from "../../components/SareeItem";
+import { useRouter } from "next/router";
+import styles from "../../styles/BrandPage.module.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+const Place = ({ data }) => {
+  const [mat, setMat] = useState();
   // let mat = 0;
   const router = useRouter();
-  var x = data.filter((item)=>(item.brand === `${router.query.id}`));
-  const [items,setItems] = useState(x);
+  var x = data.filter(
+    (item) => item.brand === `${router.query.id}` && item.imgUrl
+  );
+  const [items, setItems] = useState(x); //filter items with imgUrl
 
- 
-  const unique = [...new Set(x.map(item => item.material))];
-  
+  const unique = [...new Set(x.map((item) => item.material))];
 
-  const handleMaterial = (mater)=>{
-      mat = unique.indexOf(mater)
-      setMat(mat)
-      var list = x.filter((item)=>(item.material === `${mater}`));
-      setItems(list)
-  }
-    
-  return <div className={styles.container}>
-            <div className={styles.header}>
-            <Link href="/" passHref>
-              <div className={styles.headerItem} >
+  const handleMaterial = (mater) => {
+    mat = unique.indexOf(mater);
+    setMat(mat);
+    console.log(items);
+    var list = x.filter((item) => item.material === `${mater}`);
+    setItems(list);
+    console.log(list, "List");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.header}>
+          <Link href="/" passHref>
+            <div className={styles.headerItem}>
               <ArrowBackIcon />
-              </div>
-            </Link>
-              
-              <div className={styles.headerItem}>
-              <h2 className={styles.title}>{router.query.id.toUpperCase()}</h2>
-              </div>
-              <div className={styles.headerItem}>
-              </div>
             </div>
-            <div className={styles.filterContainer}>
-            {unique.map((item,index)=>(
-              (index === mat) ? 
-              <div className={styles.selectedfilterOptions} key={index} onClick={()=>{handleMaterial(item)}}>
-                <p>{item}</p>
-              </div> :
-              <div className={styles.filterOptions} key={index} onClick={()=>{handleMaterial(item)}}>
-                <p>{item}</p>
-              </div>
-            ))}
-            </div>
+          </Link>
 
-            {items.map((item)=>(
-              <SareeItem 
-                key = {item._id}
-                productCode = {item.productCode}
-                material = {item.material}
-                salePrice = {item.salePrice}
-                imgUrl = '/img/1.jpg'
-              />
-            )
-            )}
+          <div className={styles.headerItem}>
+            <h2 className={styles.title}>{router.query.id.toUpperCase()}</h2>
           </div>
-}
+          <div className={styles.headerItem}></div>
+        </div>
+      </div>
+      <div className={styles.filterContainer}>
+        {unique.map((item, index) =>
+          index === mat ? (
+            <div
+              className={styles.selectedfilterOptions}
+              key={index}
+              onClick={() => {
+                handleMaterial(item);
+              }}
+            >
+              <p>{item}</p>
+            </div>
+          ) : (
+            <div
+              className={styles.filterOptions}
+              key={index}
+              onClick={() => {
+                handleMaterial(item);
+              }}
+            >
+              <p>{item}</p>
+            </div>
+          )
+        )}
+      </div>
+
+      {items.map((item) => (
+        <SareeItem
+          key={item._id}
+          productCode={item.productCode}
+          material={item.material}
+          salePrice={item.salePrice}
+          imgUrl={item.imgUrl.substring(
+            item.imgUrl.indexOf("https"),
+            item.imgUrl.indexOf(")")
+          )}
+        />
+      ))}
+    </div>
+  );
+};
 export async function getStaticProps() {
   // Fetch data from external API
-  const res = await fetch(`https://retail-three.vercel.app/api/products`)
-  const data = await res.json()
+  const res = await fetch(`http://localhost:3000/api/products`);
+  const data = await res.json();
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data } };
 }
 export async function getStaticPaths() {
   // Get the paths we want to pre-render based on posts
-  const category = ['Mangalagiri', 'Coimbatore', 'Kota', 'Soft-Silk', 'Bengal', 'Venkatagiri'] 
+  const category = [
+    "Mangalagiri",
+    "Coimbatore",
+    "Kota",
+    "Soft-Silk",
+    "Bengal",
+    "Venkatagiri",
+  ];
   const paths = category.map((item) => ({
     params: { id: item },
-  }))
+  }));
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
-export default Place
+export default Place;
